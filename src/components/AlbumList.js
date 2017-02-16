22feba5166cd5184d15d73a9e7355c2b419a94e5
gus <gsuarez@imprivata.com>
2017-02-16 14:51:12 -0300

    first commit

---
+++ src/components/AlbumList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  componentWillMount() {
+    debugger;
+    console.log('componentWillMount in AlbumList');
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List!!</Text>
+      </View>
+    );
+  }
+}
+
+
+export default AlbumList;
